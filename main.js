b22988e9025b5d7322d3079bccf62083fccbc672
saanvi-maheshwari <83226597+saanvi-maheshwari@users.noreply.github.com>
2021-08-11 22:17:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var menu_array=[
+"Margherita",
+"Double Cheese Margherita",
+"Farm House",
+"Peppy Paneer",
+"Mexican Green Wave",
+"Deluxe Veggie",
+"Veg Extravaganza",
+"CHEESE N CORN"];
+function  add_top(){
+    var topping1=document.getElementById("add_toping").value;
+    console.log(topping1).push;
+    document.getElementById("topping1").innerHTML=menu_array;
+}
